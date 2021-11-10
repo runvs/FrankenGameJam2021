de1026c523c8983a2ca40fe95da345802f76d40e
Simon Weis <laguna_1989@gmx.net>
2021-11-10 13:36:37 +0100

    Tweak physics parameters a bit

--- impl/gamelib/game_properties.cpp
+++ impl/gamelib/game_properties.cpp
@@ -1,13 +1,13 @@
 ﻿#include "game_properties.hpp"
 
 int GP::RemoveBrickDeadzone() { return 32; }
-int GP::PhysicPositionIterations() { return 10; }
-int GP::PhysicVelocityIterations() { return 6; }
+int GP::PhysicPositionIterations() { return 50; }
+int GP::PhysicVelocityIterations() { return 50; }
 
 float GP::PlatformMovementSpeed() { return 65.0f; }
 float GP::PhysicsBrickDensity() { return 0.1f; }
 float GP::BrickFixVelocityThreshold() { return 42.0f; }
-float GP::PhysicsBrickRestitution() { return -0.4f; }
+float GP::PhysicsBrickRestitution() { return 0.0f; }
 float GP::AtmosphericSoundBreakpoint1() { return 0.0f; }
 float GP::AtmosphericSoundBreakpoint2() { return -300.0f; }
 float GP::AtmosphericSoundBreakpoint3() { return -600.0f; }
--- impl/gamelib/state_game.cpp
+++ impl/gamelib/state_game.cpp
@@ -503,11 +503,10 @@ void StateGame::fixCurrentBrick(std::shared_ptr<BrickInterface> currentPendingBr
 
             checkAddExtraLife(oldscore);
         }
-        currentPendingBrick->getDrawable()->flash(1.25f);
-        if (m_maxHeight <= 500) {
-            addDistanceJointsTo(currentPendingBrick, m_platform->getB2Body());
-        }
+
+        addDistanceJointsTo(currentPendingBrick, m_platform->getB2Body());
         addDistanceJointsTo(currentPendingBrick, other);
+
         currentPendingBrick->fixate();
 
         m_canSpawnNewBrick = true;
